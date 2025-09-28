1ebd32adf8206efc09879702cc4bbdd5ac50dd13
Jonathan Torres <racknarow1@gmail.com>
2025-09-28 11:56:17 -0500

    fix case name

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: 'http', hostname: 'localhost', port: '3001', pathname: '/uploads/**' },
       { protocol: 'http', hostname: '127.0.0.1', port: '3001', pathname: '/uploads/**' },
       { protocol: 'https', hostname: 'www.stl-hub.com', pathname: '/uploads/**' },
+            { protocol: 'https', hostname: 'stl-hub.com', pathname: '/uploads/**' },
     ],
   },
 };
