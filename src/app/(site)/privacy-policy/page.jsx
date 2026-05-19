8d03da4ded4eca6f0be12afb844b7539e8c40cd9
Jonathan Torres <racknarow1@gmail.com>
2026-05-19 10:35:42 -0500

    feat: create bilingual privacy policy page with responsive layout

--- src/app/(site)/privacy-policy/page.jsx
+++ src/app/(site)/privacy-policy/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import useStore from '../../../../store/useStore';
+import useStore from '@/store/useStore';
 
 export default function PrivacyPolicyPage() {
   const language = useStore((s) => s.language);
